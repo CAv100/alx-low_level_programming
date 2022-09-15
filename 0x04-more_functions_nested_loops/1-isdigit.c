96c34787c63ff4c6d23a0ba19a677894d63718bd
CAv100 <101235161+CAv100@users.noreply.github.com>
2022-09-16 00:41:12 +0100

    update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,10 @@
-
 #include "main.h"
-
 /**
  * _isdigit - checks for characters that are digits
  * @c: character 
  *
  * Return: 0 or 1
  */
-
-
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
